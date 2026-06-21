bc521ff6d25d4221d7a6f2a43e6c2f815f633a2c
Jeremias <ermias@Jeremy.home>
2026-06-21 07:00:33 -0400

    fix get id

--- admin/neighborhoods/delete.php
+++ admin/neighborhoods/delete.php
@@ -5,7 +5,7 @@ require_once '../../includes/functions.php';
 require_once '../../includes/services/NeighborhoodService.php';
 require_once '../../includes/admin-auth.php';
 
-$id = (int)($_GET['id'] ?? 0);
+$id = (int)($_POST['id'] ?? 0);
 $n  = NeighborhoodService::getById($id);
 
 if (!$n) {
